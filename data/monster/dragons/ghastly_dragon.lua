f52a94aeb5c6af79377631b71bfc5d50f1581e37
Wojciech Dybikowski <dybik08@gmail.com>
2022-04-10 12:11:19 +0200

    feat (Monsters) - ghastly dragon

--- data/monster/dragons/ghastly_dragon.lua
+++ data/monster/dragons/ghastly_dragon.lua
@@ -1,6 +1,11 @@
 local mType = Game.createMonsterType("Ghastly Dragon")
 local monster = {}
 
+monster.Credits = {
+	Developer = "Wojciech Dybikowski",
+	lastUpdate = "10/04/2022"
+}
+
 monster.description = "a ghastly dragon"
 monster.experience = 4600
 monster.outfit = {
@@ -23,8 +28,7 @@ monster.Bestiary = {
 	CharmsPoints = 50,
 	Stars = 4,
 	Occurrence = 0,
-	Locations = "Ghastly Dragon Lair, Corruption Hole, Razachai including the Inner Sanctum, \z
-		Zao Palace, Deeper Banuta, Chyllfroest."
+	Locations = "Ghastly Dragon Lair, Corruption Hole, Razachai including the Inner Sanctum, Zao Palace, Deeper Banuta, Chyllfroest."
 }
 
 monster.health = 7800
@@ -79,87 +83,56 @@ monster.voices = {
 }
 
 monster.loot = {
-	{name = "gold coin", chance = 33725, maxCount = 100},
-	{name = "gold coin", chance = 33725, maxCount = 100},
-	{name = "gold coin", chance = 33725, maxCount = 66},
-	{id = 3035, name = "platinum coin", chance = 29840, maxCount = 2},
-	{name = "soul orb", chance = 12170},
-	{name = "demonic essence", chance = 8920},
-	{name = "great mana potion", chance = 30560, maxCount = 2},
-	{name = "terra legs", chance = 3130},
-	{name = "terra boots", chance = 9510},
-	{name = "great spirit potion", chance = 29460, maxCount = 2},
-	{name = "ultimate health potion", chance = 24700},
-	{id = 8896, name = "slightly rusted armor", chance = 180},
-	{name = "shiny stone", chance = 860},
-	{name = "guardian boots", chance = 200},
-	{name = "Zaoan armor", chance = 870},
-	{name = "Zaoan helmet", chance = 150},
-	{name = "Zaoan shoes", chance = 870},
-	{name = "Zaoan legs", chance = 1400},
-	{name = "drakinata", chance = 1470},
-	{name = "Zaoan sword", chance = 100},
-	{name = "twin hooks", chance = 15100},
-	{name = "Zaoan halberd", chance = 15020},
-	{name = "spellweaver's robe", chance = 690},
-	{name = "ghastly dragon head", chance = 6650},
-	{name = "undead heart", chance = 19830},
-	{name = "jade hat", chance = 810}
+	MonsterLoot:withGoldCoins(99.58, 274),
+	MonsterLoot:withPlateLegs(50.16),
+	MonsterLoot:withSmallEmerald(39.97, 5),
+	MonsterLoot:withDarkArmor(34.84),
+	MonsterLoot:withGreatManaPotion(30.08, 2),
+	MonsterLoot:withPlatinumCoins(30.05, 2),
+	MonsterLoot:withGreatSpiritPotion(29.98, 2),
+	MonsterLoot:withUltimateHealthPotion(24.92),
+	MonsterLoot:withUndeadHeart(19.83),
+	MonsterLoot:withZaoanHalberd(15.03),
+	MonsterLoot:withTwinHooks(14.71),
+	MonsterLoot:withSoulOrb(11.93),
+	MonsterLoot:withSlightlyRustedArmor(10.09),
+	MonsterLoot:withDemonicEssence(9.08),
+	MonsterLoot:new():setLoot("ghastly dragon head", 6.92),
+	MonsterLoot:withTerraLegs(3.08),
+	MonsterLoot:withDrakinata(1.58),
+	MonsterLoot:withZaoanShoes(0.98),
+	MonsterLoot:withZaoanLegs(0.97),
+	MonsterLoot:withShinyStone(0.83),
+	MonsterLoot:withZaoanArmor(0.82),
+	MonsterLoot:withJadeHat(0.8),
+	MonsterLoot:withSpellweaversRobe(0.68),
+	MonsterLoot:withZaoanHelmet(0.23),
+	MonsterLoot:withGuardianBoots(0.22),
+	MonsterLoot:withZaoanSword(0.12)
 }
 
 monster.attacks = {
-	{name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -603},
-	{name = "ghastly dragon curse", interval = 2000, chance = 5, range = 5, target = false},
-	-- poison
-	{
-		name = "condition",
-		type = CONDITION_POISON,
-		interval = 2000,
-		chance = 10,
-		minDamage = -920,
-		maxDamage = -1280,
-		range = 5,
-		effect = CONST_ME_SMALLCLOUDS,
-		target = true
-	},
-	{
-		name = "combat",
-		interval = 2000,
-		chance = 15,
-		type = COMBAT_LIFEDRAIN,
-		minDamage = -80,
-		maxDamage = -230,
-		range = 7,
-		effect = CONST_ME_MAGIC_RED,
-		target = true
-	},
-	{name = "ghastly dragon wave", interval = 2000, chance = 10, minDamage = -120, maxDamage = -250, target = false},
-	{
-		name = "combat",
-		interval = 2000,
-		chance = 15,
-		type = COMBAT_DEATHDAMAGE,
-		minDamage = -110,
-		maxDamage = -180,
-		radius = 4,
-		effect = CONST_ME_MORTAREA,
-		target = false
-	},
-	{
-		name = "speed",
-		interval = 2000,
-		chance = 20,
-		speedChange = -800,
-		range = 7,
-		effect = CONST_ME_SMALLCLOUDS,
-		target = true,
-		duration = 30000
-	}
+	-- 	Basic attack (0-470 physical)
+	CustomMonsterSpell:new():withBasicAttack():setDamageRange(0, 470):setCondition(
+		{type = CONDITION_POISON, minDamage = -920, maxDamage = -1280, interval = 4000}
+	),
+	-- Water Wave (110-300 death)
+	CustomMonsterSpell:new():withDeathDamage():withWater():setDamageRange(110, 300):withWave(),
+	-- Death Ball (80-120 death, on self)
+	CustomMonsterSpell:new():withDeathDamage():withDeath():setDamageRange(80, 120):withBall(),
+	-- Red Stars Strike (100-210 life drain, on target)
+	CustomMonsterSpell:new():withLifeDrainDamage():withRedStars():setDamageRange(100, 210):withTarget():withStrike(),
+	-- Bats Strike (on target, effect: paralyze)
+	CustomMonsterSpell:new():withParalyze():withBats():withStrike():withTarget(),
+	-- Death Strike (on target, effect: curse 1 -> 272)
+	{name = "ghastly dragon curse", interval = 2000, chance = 5, range = 5, target = false}
 }
 
 monster.defenses = {
 	defense = 35,
-	armor = 35
+	armor = 35,
+	-- Healing (170-250 heal)
+	CustomMonsterSpell:new():withHealing(170, 250)
 }
 
 monster.elements = {
